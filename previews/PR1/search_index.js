2cd09abb62035eaf8cf4ad88753425e1a0c35e8d
Documenter.jl <documenter@juliadocs.github.io>
2026-07-11 04:14:57 +0000

    build based on 7614a0e

---
+++ previews/PR1/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"category":"section","location":"#AbstractQAtlas.jl","page":"Home","text":"","title":"AbstractQAtlas.jl"},{"category":"section","location":"#Models","page":"Home","text":"","title":"Models"}]
+}
